4c450ff09ce018a43c1fcfc4a9f2c7a98eff045c
Malte Sussdorff <malte.sussdorff@cognovis.com>
2015-01-19 08:45:31 +0100

    Ensure we have an event project type
    This is needed for further progressing and budgeting
    
    Signed-off-by: Malte Sussdorff <malte.sussdorff@cognovis.com>

--- sql/postgresql/upgrade/upgrade-4.1.0.0.0-4.1.0.0.1.sql
+++ sql/postgresql/upgrade/upgrade-4.1.0.0.0-4.1.0.0.1.sql
@@ -1,63 +1,4 @@
--- @author Neophytos Demetriou
--- @creation-date 2014-10-30
--- @last-modified 2014-10-30
-
-select acs_object_type__create_type (
-        'flyhh_event',          -- object_type
-        'Flyhh - Event',        -- pretty_name
-        'Flyhh - Events',       -- pretty_plural
-        'im_biz_object',        -- supertype
-        'flyhh_events',         -- table_name
-        'event_id',             -- id_column
-        'flyhh_event',          -- pl/pgsql package_name
-        'f',                    -- abstract_p
-        null,                   -- type_extension_table
-        'flyhh_event__name'     -- name_method
-);
-
-SELECT im_category_new ('2520', 'Event', 'Intranet Project Type');
-
--- add the dynfield attributes and default to consulting project
-delete from im_dynfield_type_attribute_map where object_type_id = 2520;
-
-INSERT INTO im_dynfield_type_attribute_map
-    (attribute_id, object_type_id, display_mode, help_text,section_heading,default_value,required_p)
-select attribute_id,2520,display_mode,help_text,section_heading,default_value,required_p from im_dynfield_type_attribute_map where object_type_id = 2501;
-
-insert into acs_object_type_tables (object_type,table_name,id_column)
-values ('flyhh_event', 'flyhh_events', 'event_id');
-
-update acs_object_types set
-        status_type_table = 'flyhh_events',
-        status_column = 'status_id',
-        type_column = 'type_id'
-where object_type = 'flyhh_event';
-
----insert into im_biz_object_urls (object_type, url_type, url) values (
----'im_project','view','/flyhh/admin/participants/view?person_id=');
----insert into im_biz_object_urls (object_type, url_type, url) values (
----'im_project','edit','/flyhh/admin/participants/new?project_id=');
-
-create table flyhh_events (
-    event_id            integer
-                        constraint flyhh_events_pk
-                        primary key,
-
-    event_name          varchar(250) not null,
-
-    -- one project per event 
-    
-	project_id			integer not null
-                        constraint flyhh_event_participants__project_fk
-                        references im_projects(project_id) on delete cascade,
-
-    enabled_p           boolean not null default 'f'
-
-); 
-
--- we query for the event_name using project_id
-create index flyhh_events__project_id__idx on flyhh_events(project_id);
-
+SELECT acs_log__debug('/packages/intranet-cust-flyhh/sql/postgresql/upgrade/upgrade-4.1.0.0.0-4.1.0.0.1.sql','');
 create table flyhh_event_materials (
     event_id            integer
                         constraint flyhh_event_materials__event_id_fk
@@ -71,7 +12,7 @@ create table flyhh_event_materials (
 
     -- status is in "Confirmed", "Pending Payment", or "Partially Paid"
     num_confirmed       integer not null default 0,
-    
+
     -- status is in "Registered"
     num_registered      integer not null default 0,
 
@@ -108,7 +49,7 @@ begin
         p_project_nr,       -- project_path
         null,               -- parent_id
         p_company_id,       -- company_id
-        '2520',              -- project_type_id (=Event)
+        '102',              -- project_type_id (=Castle Camp)
         76                  -- project_status_id (=Open)
       ) into v_project_id;
 
@@ -176,3 +117,65 @@ end;
 $$ language 'plpgsql';
 
 
+-- 
+-- Add a new project type
+--
+SELECT im_category_new ('2520', 'Event', 'Intranet Project Type');
+
+create or replace function flyhh_event__new(
+    p_event_id          integer,
+    p_event_name        varchar,
+    p_company_id        integer,
+    p_project_nr        varchar,
+    p_cost_center_id    integer,
+    p_enabled_p         boolean
+) returns boolean as 
+$$
+declare
+    v_project_id        integer;
+begin
+
+    select im_project__new(
+        null,               -- project_id
+        'im_project',     -- object_type
+        now(),              -- creation_date
+        null,               -- creation_user
+        null,               -- creation_ip
+        null,               -- context_id
+        'Event Project: ' || p_event_name,   -- project_name
+        p_project_nr,       -- project_nr
+        p_project_nr,       -- project_path
+        null,               -- parent_id
+        p_company_id,       -- company_id
+        '2520',              -- project_type_id (=Event)
+        76                  -- project_status_id (=Open)
+      ) into v_project_id;
+
+    update im_projects set
+        project_cost_center_id=p_cost_center_id,
+        project_lead_id = (select company_contact_id from im_companies where company_id=p_company_id)
+    where project_id=v_project_id;
+
+    insert into flyhh_events (
+        event_id,
+        event_name,
+        project_id,
+        enabled_p
+    ) values (
+        p_event_id,
+        p_event_name,
+        v_project_id,
+        p_enabled_p
+    );
+
+    return true;
+
+end;
+$$ language 'plpgsql';
+
+-- add the dynfield attributes and default to consulting project
+delete from im_dynfield_type_attribute_map where object_type_id = 2520;
+    
+INSERT INTO im_dynfield_type_attribute_map
+    (attribute_id, object_type_id, display_mode, help_text,section_heading,default_value,required_p)
+select attribute_id,2520,display_mode,help_text,section_heading,default_value,required_p from im_dynfield_type_attribute_map where object_type_id = 2501;
